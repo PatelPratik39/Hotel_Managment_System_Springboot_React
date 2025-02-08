a4323ef196fcd824977509c860b01e933ca2f950
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-08 00:41:05 -0600

    verv

---
+++ frontend/hms/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className=' text-3xl font-bold underline bg-red-500'>
+        <h1>Hotal management system</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
