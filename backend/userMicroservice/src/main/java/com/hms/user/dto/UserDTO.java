9d360d002bcb02ae8d4f84bd6e67a417cf0a5c76
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-16 12:35:56 -0600

    detail Exception Handling

--- backend/userMicroservice/src/main/java/com/hms/user/dto/UserDTO.java
+++ backend/userMicroservice/src/main/java/com/hms/user/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.hms.user.dto;
 
 //import com.hms.user.dto.Roles;
 import com.hms.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,12 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
 
     private Long id;
+    @NotBlank(message= "Name is Mandatory ")
     private String name;
+    @NotBlank(message= "Email is Mandatory ")
+    @Email
     private String email;
+    @NotBlank(message= "Password is Mandatory ")
     private String password;
     private Roles role;
 
--- backend/userMicroservice/src/main/java/com/hms/user/utility/ExceptionControllerAdvice.java
+++ backend/userMicroservice/src/main/java/com/hms/user/utility/ExceptionControllerAdvice.java
@@ -3,14 +3,19 @@ package com.hms.user.utility;
 import com.hms.user.exception.HmsException;
 
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
@@ -30,4 +35,16 @@ public class ExceptionControllerAdvice {
        return new ResponseEntity<>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
    }
 
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<ErrorInfo> handleViolationException(Exception e){
+        String errorMessage;
+        if(e instanceof MethodArgumentNotValidException methodArgumentNotValidException){
+            errorMessage = methodArgumentNotValidException.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(","));
+        } else {
+            ConstraintViolationException constraintViolationException = (ConstraintViolationException) e;
+            errorMessage = constraintViolationException.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(","));
+        }
+        ErrorInfo errorInfo = new ErrorInfo(errorMessage, HttpStatus.BAD_REQUEST.value(), LocalDateTime.now());
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
 }
