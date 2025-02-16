62a1e397f3d2d11ced8bfa9861840bf3f105ceee
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-16 03:37:53 -0600

    User Exception Handling using Environment

--- backend/userMicroservice/src/main/java/com/hms/user/utility/ExceptionControllerAdvice.java
+++ backend/userMicroservice/src/main/java/com/hms/user/utility/ExceptionControllerAdvice.java
@@ -1,9 +1,12 @@
 package com.hms.user.utility;
 
-import lombok.extern.slf4j.Slf4j;
+import com.hms.user.exception.HmsException;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,10 +15,19 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
+    @Autowired
+    Environment environment;
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorInfo> handleException(Exception e){
         ErrorInfo errorInfo = new ErrorInfo("Some error occurred!!!  ❌", HttpStatus.INTERNAL_SERVER_ERROR.value(), LocalDateTime.now());
         return new ResponseEntity<>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+   @ExceptionHandler(HmsException.class)
+   public ResponseEntity<ErrorInfo> handleHmsException(HmsException e){
+       ErrorInfo errorInfo = new ErrorInfo(environment.getProperty(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR.value(), LocalDateTime.now());
+       return new ResponseEntity<>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
    }
 
 }
